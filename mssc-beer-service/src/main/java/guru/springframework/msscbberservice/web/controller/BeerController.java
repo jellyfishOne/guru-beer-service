cc262ab4ba77bca1bd35ccdda246304323ecd04e
jellyfishOne <jrenteria.camino@gmail.com>
2021-06-17 17:31:13 -0700

    Documenting path and query parameters in BeerControllerTest

--- mssc-beer-service/src/main/java/guru/springframework/msscbberservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbberservice/web/controller/BeerController.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbberservice.web.controller;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
--- mssc-beer-service/src/test/java/guru/springframework/msscbberservice/BeerControllerTest.java
+++ mssc-beer-service/src/test/java/guru/springframework/msscbberservice/BeerControllerTest.java
@@ -1,21 +1,31 @@
 package guru.springframework.msscbberservice;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+
 import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -25,6 +35,8 @@ import guru.springframework.msscbberservice.web.controller.BeerController;
 import guru.springframework.msscbberservice.web.model.BeerDto;
 import guru.springframework.msscbberservice.web.model.BeerStyleEnum;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "guru.springframework.msscbberservice.web.mappers")
 public class BeerControllerTest {
@@ -41,8 +53,17 @@ public class BeerControllerTest {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+        		.param("iscold", "yes")
+        		.accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        		.andDo(document("v1/beer", 
+        				pathParameters(
+        						parameterWithName("beerId").description("UUID of desired beer to get.")
+        						),
+        				requestParameters(
+        						parameterWithName("iscold").description("Is Beer Cold Query param"))
+        				));
     }
 
     @Test
